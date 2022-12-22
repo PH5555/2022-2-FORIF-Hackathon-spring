243751ddc23ffc16cb75832dc2be843167d222a9
KimJaehyunPro <kim.jaehyun.pro@gmail.com>
2022-12-22 14:53:23 +0900

    Reset

---
+++ server/WeMeetNow/src/main/java/com/example/WeMeetNow/WeMeetNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeMeetNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeMeetNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeMeetNowApplication.class, args);
+	}
+
+}
---
+++ server/WeMeetNow/src/test/java/com/example/WeMeetNow/WeMeetNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeMeetNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeMeetNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
